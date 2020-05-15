03f2d81efda80a54cc284a5270cb31a50aa7311b
Samizzo <samizzographics@gmail.com>
2020-05-15 12:52:59 +0300

    added some files

--- djangorestapi/settings.py
+++ djangorestapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ez5t794z5(7qigfxz6pgcw9&_nya5-x(kj#dp)$z(=kt$oxmfe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['samizzodjangorestapi.herokuapp.com]python','127.0.0.1']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangorestapi.urls'
